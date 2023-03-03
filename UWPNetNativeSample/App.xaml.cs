8ffd8b440e7bbe29565c41f6c0c83c2622672b28
Gustave Monce <gustave.monce@outlook.com>
2023-03-03 21:00:18 +0100

    Add sample application

---
+++ UWPNetNativeSample/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace UWPNetNativeSample
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            if (!(Window.Current.Content is MainPage))
+            {
+                MainPage mainPage = new MainPage();
+                Window.Current.Content = mainPage;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                Window.Current.Activate();
+            }
+        }
+    }
+}
---
+++ UWPNetNativeSample/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using RegistryRT;
+
+namespace UWPNetNativeSample
+{
+    public sealed partial class MainPage : Page
+    {
+        private static readonly Registry registry = new Registry();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            uint CurrentMajorVersionNumber = 0;
+            uint CurrentMinorVersionNumber = 0;
+            string CurrentBuild = "";
+
+            byte[] valueBuffer;
+            RegistryType regType;
+
+            bool result = registry.QueryValue(RegistryHive.HKEY_LOCAL_MACHINE,
+                                              @"SOFTWARE\Microsoft\Windows NT\CurrentVersion",
+                                              "CurrentBuild",
+                                              out regType,
+                                              out valueBuffer);
+
+            if (result && regType == RegistryType.String)
+            {
+                CurrentBuild = System.Text.Encoding.ASCII.GetString(valueBuffer);
+            }
+
+            result = registry.QueryValue(RegistryHive.HKEY_LOCAL_MACHINE,
+                                         @"SOFTWARE\Microsoft\Windows NT\CurrentVersion",
+                                         "CurrentMajorVersionNumber",
+                                         out regType,
+                                         out valueBuffer);
+
+            if (result && regType == RegistryType.Integer)
+            {
+                CurrentMajorVersionNumber = BitConverter.ToUInt32(valueBuffer, 0);
+            }
+
+            result = registry.QueryValue(RegistryHive.HKEY_LOCAL_MACHINE,
+                                         @"SOFTWARE\Microsoft\Windows NT\CurrentVersion",
+                                         "CurrentMinorVersionNumber",
+                                         out regType,
+                                         out valueBuffer);
+
+            if (result && regType == RegistryType.Integer)
+            {
+                CurrentMinorVersionNumber = BitConverter.ToUInt32(valueBuffer, 0);
+            }
+
+            BuildNumberRun.Text = $"Microsoft (R) Windows (R) Version {CurrentMajorVersionNumber}.{CurrentMinorVersionNumber} (Build {CurrentBuild})";
+
+            bool success = registry.QueryValue(RegistryHive.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", "BuildLabEx", out RegistryType _, out byte[] rawData);
+            if (success)
+            {
+                string buildStringLabEx = System.Text.Encoding.ASCII.GetString(rawData);
+            }
+        }
+    }
+}
